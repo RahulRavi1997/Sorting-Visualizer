63273581898deeca3e64cb942bff887244e56d00
RahulRavi1997 <sriramravi97@gmail.com>
2020-03-28 18:37:53 +0530

    Dummy commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Layout from './containers/Layout';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header">
-      Sorting Visualizer
+    <BrowserRouter basename="folder1">
+          <Route exact path="/"  component={App1} />
+    </BrowserRouter>
+  )
+}
+  function App1() {
+  return (
+      <div className="app">
+        <header className="app-header">
+          Sorting Visualizer
       </header>
-      <Layout />
-    </div>
+        <Layout />
+      </div>
   );
 }
 
